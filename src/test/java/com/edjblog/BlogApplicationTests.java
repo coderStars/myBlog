bdd983b58f268bff9209eb256a9c5437028a8006
AlbertBoys <1430292539@qq.com>
2019-12-31 13:39:07 +0800

    tijiao

---
+++ src/test/java/com/edjblog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edjblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
