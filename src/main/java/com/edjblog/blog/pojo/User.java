c3b599907dc48975669dab8984f62c69413c87e4
AlbertBoys <1430292539@qq.com>
2019-12-31 12:20:35 +0800

    pojo层

---
+++ src/main/java/com/edjblog/blog/pojo/User.java
@@ -0,0 +1,13 @@
+package com.edjblog.blog.pojo;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel("用户实体")
+public class User {
+    @ApiModelProperty("用户ID")
+    private Integer id;
+    @ApiModelProperty("用户姓名")
+    private String name;
+}
